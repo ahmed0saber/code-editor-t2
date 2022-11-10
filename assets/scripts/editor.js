7cdd1c38ff00c928b8faea183a9855249cef7955
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2022-11-10 02:14:36 +0200

    create code js to run editor
    create code for make editor run

--- assets/scripts/editor.js
+++ assets/scripts/editor.js
@@ -37,3 +37,16 @@ function show() {
   frame.style.display = "none";
   content.style.display = "block";
 }
+ /*كود ترميز النصوص */
+document.getElementById('html').value="<div>\n\n</div>";
+document.getElementById('css').value="<style>\n\n</style>";
+document.getElementById('js').value="<script>\n\n</script>";
+function showcode(){
+  var htmlcode=document.getElementById('html').value;
+  var csscode=document.getElementById('css').value + "";
+  var jscode=document.getElementById('js').value + "";
+  var output=document.getElementById('iframe').contentWindow.document;
+  output.open();
+  output.write(htmlcode+csscode+jscode);
+  output.close();
+}
